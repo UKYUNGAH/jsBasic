a642732869b4e64266d2bbfddd62b08a9d03db4a
KYUNGAH <ukyunga0.1@gmail.com>
2024-01-19 17:52:35 +0900

    re test;

--- Level 3/3-9.js
+++ Level 3/3-9.js
@@ -5,22 +5,54 @@ var products = [
     { id : 2, price : 60000, title : 'Black Monastery' }
   ];
 
-var row = document.querySelectorAll('.row');
-var card = `<div class="col-sm-4">
-<img src="https://via.placeholder.com/600" class="w-100">
-<h5>${products[]}</h5>
-<p>가격 : 70000</p>
-</div>`
 
-for(let i = 0; i < 3; i++){
-    let card = `<div class="col-sm-4">
-    <img src="https://via.placeholder.com/600" class="w-100">
-    <h5>${products[i].title}</h5>
-    <p>가격 : ${products[i].price}</p>
-    </div>`
-    
-    row[0].insertAdjacentHTML('beforeend', card)
-    // for(let j = 0; j < i.length; j++){
-    //     row[0].innerHTML = products.price
-    // }
-}
+
+// 숙제 상품목록 3개 만들어오기 자바스크립트로 html 생성해서
+
+var row = document.querySelector('.row');
+var container = document.querySelector('.container');
+var btn = document.querySelector('#more');
+
+// var 템플릿 = 
+// `<div class="col-sm-4">
+// <img src="https://via.placeholder.com/600" class="w-100">
+// <h5>${products[0].title}</h5>
+// <p>가격 : ${products[0].price}</p>
+// </div>`;
+
+// // $('.row').append(템플릿)
+// row.insertAdjacentHTML('beforeend', 템플릿);
+
+products.forEach((a, i)=>{
+  var 템플릿 = 
+  `<div class="col-sm-4">
+  <img src="https://via.placeholder.com/600" class="w-100">
+  <h5>${a.title}</h5>
+  <p>가격 : ${a.price}</p>
+  </div>`;
+
+  // <h5>${a.title}</h5> 을 해줘도됨
+  row.insertAdjacentHTML('beforeend', 템플릿);
+})
+
+
+var count = 0;
+btn.addEventListener('click', function(data){
+  count++
+  fetch(`https://codingapple1.github.io/js/more${count}.json`)
+    .then(res => res.json())
+    .then(data => {
+      // 3개 생성하기
+      data.forEach((a, i) =>{
+        var 템플릿 = 
+        `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${a.title}</h5>
+        <p>가격 : ${a.price}</p>
+        </div>`;
+      
+        row.insertAdjacentHTML('beforeend', 템플릿);
+      })
+    })
+
+})
